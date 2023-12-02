df5b85b2f987ee6ee36c47252abba24c7599df3d
RiverSpider <raevskih8888@mail.ru>
2023-12-03 01:00:50 +0600

    add: all work done

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,38 @@
 import 'package:digital_department_flutter/mainsection/navigationbuttons.dart';
 import 'package:flutter/material.dart';
-import 'package:digital_department_flutter/mainsection/background.dart';
 import 'package:digital_department_flutter/topsection/topsection.dart';
 
 import 'Assets/Consts.dart';
-import 'mainsection/mainsection.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Column(
-        children: [
-          TopSection(),
-          Expanded(
-            child: Navigationbuttons(),
+      home: DefaultTabController(
+        length: 1,
+        child: Scaffold(
+          body: NestedScrollView(
+            headerSliverBuilder: (context, innerBoxIsScrolled) {
+              return [
+                const SliverAppBar(
+                  expandedHeight: AppConstants.topsectionheight-24,
+                  flexibleSpace: FlexibleSpaceBar(
+                    background: TopSection(),
+                  ),
+                ),
+              ];
+            },
+            body: const TabBarView(
+              children: [
+                Navigationbuttons(),
+              ],
+            ),
           ),
-        ],
+        ),
       ),
     );
   }
--- lib/mainsection/mainsection.dart
+++ lib/mainsection/mainsection.dart
@@ -47,161 +47,159 @@ class MainSection extends StatelessWidget {
       {'title': StringResources.science},
     ];
 
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          SizedBox(
-            child: Stack(
-              children: [
-                const Positioned(
-                  child: Background(),
-                ),
-                Column(
-                  children: [
-                    FontThemes.spacermedium,
-                    const SectionHeader(title: StringResources.subheader, description: StringResources.subdescription),
-                    Material(
-                      child: SizedBox(
-                        height: AppConstants.cardheight,
-                        child: ListView(
-                          scrollDirection: Axis.horizontal,
-                          shrinkWrap: true,
-                          children: <Widget>[
-                            const SizedBox(width: AppConstants.cardimagesize),
-                            ...cardDataList.map((cardData) {
-                              return Row(
-                                children: [
-                                  Stack(
-                                    children: [
-                                      Cards(
-                                        title: cardData['title'] ?? '',
-                                        description: cardData['description'] ?? '',
-                                        subdiscription: cardData['subdiscription'] ?? '',
-                                        image: cardData['image'] ?? '',
-                                      ),
-                                      Positioned.fill(
-                                        child: InkWell(
-                                          onTap: () {
+    return ListView(children: [Column(
+      children: [
+        SizedBox(
+          child: Stack(
+            children: [
+              const Positioned(
+                child: Background(),
+              ),
+              Column(
+                children: [
+                  FontThemes.spacermedium,
+                  const SectionHeader(title: StringResources.subheader, description: StringResources.subdescription),
+                  Material(
+                    child: SizedBox(
+                      height: AppConstants.cardheight,
+                      child: ListView(
+                        scrollDirection: Axis.horizontal,
+                        shrinkWrap: true,
+                        children: <Widget>[
+                          const SizedBox(width: AppConstants.cardimagesize),
+                          ...cardDataList.map((cardData) {
+                            return Row(
+                              children: [
+                                Stack(
+                                  children: [
+                                    Cards(
+                                      title: cardData['title'] ?? '',
+                                      description: cardData['description'] ?? '',
+                                      subdiscription: cardData['subdiscription'] ?? '',
+                                      image: cardData['image'] ?? '',
+                                    ),
+                                    Positioned.fill(
+                                      child: InkWell(
+                                        onTap: () {
 
-                                          },
-                                        ),
+                                        },
                                       ),
-                                    ],
-                                  ),
-                                  FontThemes.spacersmall,
-                                ],
-                              );
-                            }).toList(),
-                            FontThemes.spacerlarge,
-                          ],
-                        ),
-                      ),
-                    ),
-                    Material(
-                      child: Column(
-                        children: [
-                          FontThemes.spacermedium,
-                          const SectionHeader(
-                            title: StringResources.ratesheader,
-                            description: StringResources.ratesdescription,
-                          ),
-                          Stack(
-                            children: [
-                              // Ваш виджет Disclosure
-                              const Disclosure(
-                                title: StringResources.limitheader,
-                                description: StringResources.limitdescription,
-                                image: StringResources.speedpic,
-                              ),
-                              // InkWell поверх виджета
-                              Positioned.fill(
-                                child: Material(
-                                  color: Colors.transparent,
-                                  child: InkWell(
-                                    onTap: () {
-                                      // Действие при нажатии на кнопку Disclosure
-                                    },
-                                  ),
+                                    ),
+                                  ],
                                 ),
-                              ),
-                            ],
-                          ),
-                          FontThemes.spacersmall,
-                          FontThemes.divider,
-                          Stack(
-                            children: [
-                              // Ваш виджет Disclosure
-                              const Disclosure(
-                                title: StringResources.transferheader,
-                                description: StringResources.transferdescription,
-                                image: StringResources.percentpic,
-                              ),
-                              // InkWell поверх виджета
-                              Positioned.fill(
-                                child: Material(
-                                  color: Colors.transparent,
-                                  child: InkWell(
-                                    onTap: () {
-                                      // Действие при нажатии на кнопку Disclosure
-                                    },
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                          FontThemes.spacersmall,
-                          FontThemes.divider,
-                          Stack(
-                            children: [
-                              // Ваш виджет Disclosure
-                              const Disclosure2(
-                                title: StringResources.infoheader,
-                                image: StringResources.forwardpic,
-                              ),
-                              // InkWell поверх виджета
-                              Positioned.fill(
-                                child: Material(
-                                  color: Colors.transparent,
-                                  child: InkWell(
-                                    onTap: () {
-                                      // Действие при нажатии на кнопку Disclosure
-                                    },
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
+                                FontThemes.spacersmall,
+                              ],
+                            );
+                          }).toList(),
+                          FontThemes.spacerlarge,
                         ],
                       ),
                     ),
-                    FontThemes.spacersmall,
-                    const SectionHeader(title: StringResources.interestsheader, description: StringResources.interestsdescription),
-                    Material(
-                      child: Container(
-                        padding: const EdgeInsets.only(left: AppConstants.chipspacerleft, right: AppConstants.chipspacerright),
-                        child: Wrap(
-                          spacing: AppConstants.chippadding,
-                          runSpacing: AppConstants.chippadding,
-                          children: fillDataList.map((fillData) {
-                            return InkWell(
-                              onTap: () {
-                                print('${fillData['title']} tapped');
-                              },
-                              child: Fill(
-                                title: fillData['title'] ?? '',
-                              ),
-                            );
-                          }).toList(),
+                  ),
+                  Material(
+                    child: Column(
+                      children: [
+                        FontThemes.spacermedium,
+                        const SectionHeader(
+                          title: StringResources.ratesheader,
+                          description: StringResources.ratesdescription,
                         ),
+                        Stack(
+                          children: [
+                            // Ваш виджет Disclosure
+                            const Disclosure(
+                              title: StringResources.limitheader,
+                              description: StringResources.limitdescription,
+                              image: StringResources.speedpic,
+                            ),
+                            // InkWell поверх виджета
+                            Positioned.fill(
+                              child: Material(
+                                color: Colors.transparent,
+                                child: InkWell(
+                                  onTap: () {
+                                    // Действие при нажатии на кнопку Disclosure
+                                  },
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        FontThemes.spacersmall,
+                        FontThemes.divider,
+                        Stack(
+                          children: [
+                            // Ваш виджет Disclosure
+                            const Disclosure(
+                              title: StringResources.transferheader,
+                              description: StringResources.transferdescription,
+                              image: StringResources.percentpic,
+                            ),
+                            // InkWell поверх виджета
+                            Positioned.fill(
+                              child: Material(
+                                color: Colors.transparent,
+                                child: InkWell(
+                                  onTap: () {
+                                    // Действие при нажатии на кнопку Disclosure
+                                  },
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        FontThemes.spacersmall,
+                        FontThemes.divider,
+                        Stack(
+                          children: [
+                            // Ваш виджет Disclosure
+                            const Disclosure2(
+                              title: StringResources.infoheader,
+                              image: StringResources.forwardpic,
+                            ),
+                            // InkWell поверх виджета
+                            Positioned.fill(
+                              child: Material(
+                                color: Colors.transparent,
+                                child: InkWell(
+                                  onTap: () {
+                                    // Действие при нажатии на кнопку Disclosure
+                                  },
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  FontThemes.spacersmall,
+                  const SectionHeader(title: StringResources.interestsheader, description: StringResources.interestsdescription),
+                  Material(
+                    child: Container(
+                      padding: const EdgeInsets.only(left: AppConstants.chipspacerleft, right: AppConstants.chipspacerright),
+                      child: Wrap(
+                        spacing: AppConstants.chippadding,
+                        runSpacing: AppConstants.chippadding,
+                        children: fillDataList.map((fillData) {
+                          return InkWell(
+                            onTap: () {
+                              print('${fillData['title']} tapped');
+                            },
+                            child: Fill(
+                              title: fillData['title'] ?? '',
+                            ),
+                          );
+                        }).toList(),
                       ),
                     ),
-                  ],
-                ),
-              ],
-            ),
+                  ),
+                ],
+              ),
+            ],
           ),
-        ],
-      ),
-    );
+        ),
+      ],
+    )],);
   }
 }
